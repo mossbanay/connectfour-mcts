1522156eed73c5045761f74a4799b7f45a676302
leaen <mossbanay@gmail.com>
2020-07-04 11:45:18 +1000

    updated training loop

--- src/main.py
+++ src/main.py
@@ -9,37 +9,48 @@ from tqdm import trange
 from agents import Agent, RandomAgent
 
 
-def play_game(env, first_agent: Agent, second_agent: Agent):
+def play_game(env, agents):
     """Pit two agents again each other, returning the final reward of the game"""
 
-    observation = env.reset()
-    timestep = (observation, 0, False, {})
-
-    first_agent.observe_first(timestep)
-    action = first_agent.select_action(observation)
-
-    timestep = env.step(action)
+    timestep = (env.reset(), 0, False, {})
     observation, reward, done, info = timestep
-
-    first_agent.observe(action, timestep)
-    second_agent.observe_first(timestep)
-
-    action = second_agent.select_action(observation)
-    timestep = env.step(action)
-    observation, reward, done, info = timestep
-
-    second_agent.observe(action, timestep)
-
+    last_action = []
     agent_idx = 0
-    agents = [first_agent, second_agent]
-    while not done:
-        action = agents[agent_idx].select_action(observation)
+    steps = 0
+
+    for agent in agents:
+        agent.observe_first(timestep)
+
+        action = agent.select_action(observation)
+        while action not in env.legal_moves():
+            action = agent.select_action(observation)
+        last_action.append(action)
+
         timestep = env.step(action)
         observation, reward, done, info = timestep
-        agents[agent_idx].observe(action, timestep)
-        agent_idx = (agent_idx + 1) % 2
 
-    return reward
+        steps += 1
+
+    while not done:
+        agent = agents[agent_idx]
+
+        agent.observe(last_action[agent_idx], timestep)
+
+        action = agent.select_action(observation)
+        while action not in env.legal_moves():
+            action = agent.select_action(observation)
+        last_action[agent_idx] = action
+
+        timestep = env.step(action)
+        observation, reward, done, info = timestep
+
+        agent_idx = (agent_idx + 1) % len(agents)
+        steps += 1
+
+    return {
+        'reward': reward,
+        'steps': steps,
+        }
 
 def main():
     parser = argparse.ArgumentParser()
@@ -53,14 +64,15 @@ def main():
 
     data = []
     for i in trange(args.n_games):
-        reward = play_game(env, first_agent, second_agent)
+        result = play_game(env, [first_agent, second_agent])
         data.append({
             'game_idx': i,
-            'reward': reward,
+            **result,
         })
 
     df = pd.DataFrame(data)
     print(df.describe())
+    print(df['reward'].value_counts())
 
 
 if __name__ == '__main__':
