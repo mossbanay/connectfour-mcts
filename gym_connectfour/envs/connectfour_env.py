f77412590b41691b150d379f3057bd80ee7803a3
leaen <mossbanay@gmail.com>
2020-07-23 21:17:10 +1000

    begin refactor into dm_env

--- gym_connectfour/envs/connectfour_env.py
+++ gym_connectfour/envs/connectfour_env.py
@@ -1,5 +1,7 @@
-import gym
-from gym import spaces
+import dm_env
+from dm_env import specs
+
+import numpy as np
 
 
 N_WIDTH = 7
@@ -7,20 +9,22 @@ N_HEIGHT = 6
 N_STREAK_WIN = 4
 
 
-class ConnectFourEnv(gym.Env):
-    metadata = {'render.modes': ['ansi']}
+class ConnectFourEnv(dm_env.Environment):
+    metadata = {"render.modes": ["ansi"]}
     action_space = spaces.Discrete(N_WIDTH)
-    observation_space = spaces.Tuple((spaces.MultiDiscrete([N_HEIGHT, N_WIDTH, 3]), spaces.Discrete(2)))
+    observation_space = spaces.Tuple(
+        (spaces.MultiDiscrete([N_HEIGHT, N_WIDTH, 3]), spaces.Discrete(2))
+    )
     reward_range = (-1, 1)
 
     def __init__(self):
         self.player_one_turn = True
-        self.board = []
+        self.board = np.zeros((2, N_WIDTH, N_HEIGHT))
         self.reset()
 
     def step(self, action):
         """Play out one action in the environment"""
-        
+
         for i in range(N_HEIGHT):
             if self.board[i][action] == 0:
                 self.board[i][action] = 1 if self.player_one_turn else 2
@@ -36,20 +40,24 @@ class ConnectFourEnv(gym.Env):
 
         return (observation, reward, done, info)
 
+    def _observation(self):
+        return self.board.copy()
+
     def reset(self):
-        """Reset the environment"""
+        """Returns the first `TimeStep` of a new episode."""
 
         self.player_one_turn = True
         self.board = [[0 for _ in range(N_WIDTH)] for _ in range(N_HEIGHT)]
+        dm_env.restart(self._observation())
 
     def legal_moves(self):
         """Find the current moves that are legal"""
 
         moves = []
         for i in range(N_WIDTH):
-            if self.board[N_HEIGHT-1][i] == 0:
+            if self.board[N_HEIGHT - 1][i] == 0:
                 moves.append(i)
-        
+
         return moves
 
     def winner(self):
@@ -64,12 +72,12 @@ class ConnectFourEnv(gym.Env):
                     streak += 1
                 else:
                     streak = 0
-                
+
                 if current != 0 and streak == N_STREAK_WIN:
                     return last
 
                 last = current
-        
+
         # Check for a vertical win
         for j in range(N_WIDTH):
             streak = 0
@@ -80,14 +88,14 @@ class ConnectFourEnv(gym.Env):
                     streak += 1
                 else:
                     streak = 1
-                
+
                 if current != 0 and streak == N_STREAK_WIN:
                     return last
 
                 last = current
-        
+
         # Check upward sloping diagonals
-        for i in range(N_HEIGHT-1, 0, -1):
+        for i in range(N_HEIGHT - 1, 0, -1):
             j = 0
             streak = 0
             last = 0
@@ -97,14 +105,14 @@ class ConnectFourEnv(gym.Env):
                     streak += 1
                 else:
                     streak = 1
-                
+
                 if current != 0 and streak == N_STREAK_WIN:
                     return last
 
                 last = current
                 i += 1
                 j += 1
-        
+
         for j in range(N_WIDTH):
             i = 0
             streak = 0
@@ -115,17 +123,17 @@ class ConnectFourEnv(gym.Env):
                     streak += 1
                 else:
                     streak = 1
-                
+
                 if current != 0 and streak == N_STREAK_WIN:
                     return last
 
                 last = current
                 i += 1
                 j += 1
-  
+
         # Check downward sloping diagonals
-        for i in range(0, N_HEIGHT-1):
-            j = N_WIDTH-1
+        for i in range(0, N_HEIGHT - 1):
+            j = N_WIDTH - 1
             streak = 0
             last = 0
             while i > 0 and j > 0:
@@ -134,16 +142,16 @@ class ConnectFourEnv(gym.Env):
                     streak += 1
                 else:
                     streak = 1
-                
+
                 if current != 0 and streak == N_STREAK_WIN:
                     return last
 
                 last = current
                 i -= 1
                 j -= 1
-        
+
         for j in range(N_WIDTH):
-            i = N_HEIGHT-1
+            i = N_HEIGHT - 1
             streak = 0
             last = 0
             while i > 0 and j > 0:
@@ -152,38 +160,42 @@ class ConnectFourEnv(gym.Env):
                     streak += 1
                 else:
                     streak = 1
-                
+
                 if current != 0 and streak == N_STREAK_WIN:
                     return last
 
                 last = current
                 i -= 1
                 j -= 1
-        
+
         return None
 
-    def render(self, mode='ansi', close=False):
+    def render(self, mode="ansi", close=False):
         """Render the board as a string"""
 
-        if mode != 'ansi':
+        if mode != "ansi":
             raise NotImplementedError("Only ansi render mode is supported.")
 
         num_to_char = {
-            0: ' ',
-            1: '1',
-            2: '2',
+            0: " ",
+            1: "1",
+            2: "2",
         }
 
         rows = []
 
-        next_to_move = 'Player 1' if self.player_one_turn else 'Player 2'
-        rows.append(f'{next_to_move} to move')
+        next_to_move = "Player 1" if self.player_one_turn else "Player 2"
+        rows.append(f"{next_to_move} to move")
 
-        rows.append('╚' + '═══╩'*(N_WIDTH-1) + '═══╝')
-        rows.extend(['║ ' + ' ║ '.join(map(lambda x: num_to_char[x], row)) + ' ║' for row in self.board])
+        rows.append("╚" + "═══╩" * (N_WIDTH - 1) + "═══╝")
+        rows.extend(
+            [
+                "║ " + " ║ ".join(map(lambda x: num_to_char[x], row)) + " ║"
+                for row in self.board
+            ]
+        )
 
-
-        return '\n'.join(rows[::-1])
+        return "\n".join(rows[::-1])
 
     def set_state(self, observation):
         raise NotImplementedError()
--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
 from setuptools import setup
- 
-setup(name='gym_connectfour',
-      version='0.1',
-      install_requires=['gym']
-)
+
+setup(name="gym_connectfour", version="0.1", install_requires=["dm_env", "numpy"])
