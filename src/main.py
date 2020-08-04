7e154732300e54acdc64c8677ad7f07716edba87
leaen <mossbanay@gmail.com>
2020-08-04 20:33:08 +1000

    run rounds in parallel

--- src/main.py
+++ src/main.py
@@ -4,8 +4,9 @@ import argparse
 import pandas as pd
 import seaborn as sns
 
-from tqdm import trange
-
+from multiprocessing import Pool
+from tqdm import tqdm
+from functools import partial
 from agents import Agent, RandomAgent, MCTSAgent
 
 
@@ -35,24 +36,37 @@ def play_game(env, agent, opponent_agent=None):
     }
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--n-games", type=int, default=25)
-    parser.add_argument("--n-rounds", type=int, default=100)
-
-    args = parser.parse_args()
-
+def task(round_n, n_games=25):
     env = gym_connectfour.envs.ConnectFourEnv()
     random_agent = RandomAgent(env.action_spec())
 
+    agent = MCTSAgent(env.action_spec(), time_budget=0.001)
+
     data = []
-    for round_n in trange(args.n_rounds):
-        agent = MCTSAgent(env.action_spec(), time_budget=0.005)
 
-        for game_n in range(args.n_games):
-            results = play_game(env, agent, opponent_agent=random_agent)
+    for game_n in range(n_games):
+        results = play_game(env, agent, opponent_agent=random_agent)
 
-            data.append({"round_n": round_n, "game_n": game_n, **results})
+        data.append({"round_n": round_n, "game_n": game_n, **results})
+
+    return data
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n-games", type=int, default=25)
+    parser.add_argument("--n-rounds", type=int, default=50)
+    parser.add_argument("--n-threads", type=int, default=1)
+
+    args = parser.parse_args()
+
+    pool = Pool(args.n_threads)
+    data = []
+    t = partial(task, n_games=args.n_games)
+    for step_data in tqdm(
+        pool.imap_unordered(t, range(args.n_rounds)), total=args.n_rounds
+    ):
+        data.extend(step_data)
 
     df = pd.DataFrame(data)
 
